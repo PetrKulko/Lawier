987a600cae3ea151aa37fe27d73cb81325d6cd1a
Petr_Kulko <petrokulko@gmail.com>
2021-07-30 16:32:05 +0300

    create progect

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let btnOpen = document.getElementById('btnOpen');
+btnClose = document.getElementById('btnClose');
+exampleModal = document.getElementById('exampleModal');
+
+const closeBtn = n => {
+    btnOpen.style = "display: none;";
+}
+
+const remoteBtn = n => {
+    btnOpen.style = "display: block;";
+}
+
+btnOpen.addEventListener('click', closeBtn);
+btnClose.addEventListener('click', remoteBtn);
+exampleModal.addEventListener('click', remoteBtn);
